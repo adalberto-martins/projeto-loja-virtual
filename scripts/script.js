89505a61395757155d86a5bbda4d77014605e190
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2025-11-13 06:52:51 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -231,7 +231,7 @@ function loadAdminProducts() {
         <div class="buy">
           <button class="btn" onclick="openWhatsApp(${p.nome})">WhatsApp</button>
           <button class="btn btn-buy">Adicionar</button>
-          <button class="btn secondary" onclick="openPixFromButton('${p.nome},${Number(p.preco).toFixed(2)}')">Pagar com Pix</button>
+          <button class="btn secondary" onclick="openPixFromButton('${p.nome},${Number(p.preco).toFixed(2).replace('.',',')}')">Pagar com Pix</button>
         </div>
       `;
       grid.appendChild(card);
